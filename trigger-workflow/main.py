66d4ed58e6bc6d195c1713850ec3b1a66d037be3
ormbo <or.bohadana@midlink.co.il>
2026-02-12 14:30:05 +0200

    Update

--- trigger-workflow/main.py
+++ trigger-workflow/main.py
@@ -7,7 +7,8 @@ def main():
 
     my_output = f"The workflow file {workflow_file}"
 
-    print(f"::set-output name=myOutput::{my_output}")
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+        fh.write(f'myOutput={workflow_file}')
 
 
 if __name__ == "__main__":
